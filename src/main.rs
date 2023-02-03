9d2a3956c83d51430c66cb00ea5204269f5ef844
Heylium <tuanhsiang@163.com>
2023-02-03 23:38:31 +0800

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,19 @@ fn main() {
     // Uncomment this block to pass the first stage
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    loop {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    let mut buf = [0;512];
+                    stream.read(&mut buf).unwrap();
+                    stream.write("+PONG\r\n".as_bytes()).unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buf = [0;512];
-                stream.read(&mut buf).unwrap();
-                stream.write("+PONG\r\n".as_bytes()).unwrap();
-
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
+                    println!("accepted new connection");
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
         }
     }
