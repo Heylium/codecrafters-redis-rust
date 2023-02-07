3f29252318e6f257471a5c7303bf60fd7c1135c4
Heylium <tuanhsiang@163.com>
2023-02-08 00:04:23 +0800

    pass 6th stage

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,6 @@ async fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
     let main_store = Arc::new(Mutex::new(Store::new()));
 
